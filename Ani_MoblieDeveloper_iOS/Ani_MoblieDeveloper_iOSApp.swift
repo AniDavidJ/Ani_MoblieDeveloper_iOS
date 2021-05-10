63c9cc6cfe3b849d446be6e37681349ae50543dd
AniDavid <anidavid07@gmail.com>
2021-05-10 18:29:16 +0400

    Initial Commit

---
+++ Ani_MoblieDeveloper_iOS/Ani_MoblieDeveloper_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ani_MoblieDeveloper_iOSApp.swift
+//  Ani_MoblieDeveloper_iOS
+//
+//  Created by ani david on 10/05/21.
+//
+
+import SwiftUI
+
+@main
+struct Ani_MoblieDeveloper_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
